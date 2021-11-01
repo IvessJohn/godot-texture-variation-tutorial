19c054a0e1be4faffc9a823ef935a04f55330883
IvessJohn <59248545+IvessJohn@users.noreply.github.com>
2021-11-01 09:54:46 +0200

    Method 1 implementation

--- Sprite.gd
+++ Sprite.gd
@@ -1,8 +1,17 @@
 extends Sprite
 
+export(Array, Texture) var TEXTURE_VARIATIONS_ARRAY: Array = [
+	preload("res://textures/variation1.png"),
+	preload("res://textures/variation2.png"),
+	preload("res://textures/variation3.png")
+]
 
 func _ready():
 	variate_texture()
 
 func variate_texture():
-	pass
+	# Separate files approach
+	if TEXTURE_VARIATIONS_ARRAY.size() > 1:
+		var texture_id: int = randi() % TEXTURE_VARIATIONS_ARRAY.size()
+		var chosen_texture: Texture = TEXTURE_VARIATIONS_ARRAY[texture_id]
+		texture = chosen_texture
